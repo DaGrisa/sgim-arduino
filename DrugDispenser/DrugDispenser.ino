960c9854446b869940abf7e6e7dc1ea6a20a710f
daniel <dgr7348@students.fhv.at>
2018-02-26 16:31:23 +0100

    commands

--- DrugDispenser/DrugDispenser.ino
+++ DrugDispenser/DrugDispenser.ino
@@ -1,54 +1,89 @@
 #include <Servo.h>
 
+//## PARAMETERS ##//
+bool DEBUG = false;
+// servo dispenser
 int servoPinFirstDispenser = 9;
 Servo servo;
 int servoAngle = 0;
+// vibration dispenser
+int motorPinSecondDispenser = 13;
+int photoResistorPin  = 0; 
+int photoResistorValue = 0;
+int photoResistorActivateValue = 900;
 
-String lineRead;
+//## GLOBAL VARIABLES ##//
 int dispenserId;
 int amount;
+String commands[10];
+int commandsIndex = -1;
+bool runningFlag;
 
-int photoResistorPin = 0; 
-int photoResistorValue = 0;
-
+//## MAIN ##//
 void setup() {
   Serial.begin(9600);  
+  pinMode(motorPinSecondDispenser, OUTPUT); 
 }
-
 void loop() {
   
   if (Serial.available() > 0) {
     // read the incoming byte:
-    lineRead = Serial.readString();
-                
-    int delimiterIndex = lineRead.indexOf(';');
-    dispenserId = lineRead.substring(0, delimiterIndex).toInt();
-    amount = lineRead.substring(delimiterIndex+1).toInt();
-
-    dispense(dispenserId, amount);
+    readCommands();
+  } else if (commandsIndex > -1) {
+    String command = commands[commandsIndex--];
+    runCommand(command);
   }
-
-  photoResistorValue = analogRead(photoResistorPin);
-  if(photoResistorValue >= 800){
-    Serial.println("Photo Resistor Activation!");
-  }
-    
+}
+void runCommand(String lineRead){
+  runningFlag = true;
+      
+  int delimiterIndex = lineRead.indexOf(';');
+  dispenserId = lineRead.substring(0, delimiterIndex).toInt();
+  amount = lineRead.substring(delimiterIndex+1).toInt();
+  
+  dispense(dispenserId, amount);
+  runningFlag = false;
 }
 
-void dispense(int dispenserId, int amount) {
-  Serial.print("dispenser ");
-  Serial.print(dispenserId);
-  Serial.print(" with amount ");
-  Serial.println(amount);
+//## HELPER ##//
+void readCommands() {
+  String lineRead = Serial.readString();
 
-  int servoPin;
+  int startIndex = 0;
+  int delimiterIndex = 0;
+ 
+  while(delimiterIndex >= 0){
+    delimiterIndex = lineRead.indexOf('-', startIndex);
+    String command = lineRead.substring(startIndex, delimiterIndex);
+    if(command.length()>0){
+      commandsIndex++;
+      commands[commandsIndex] = command;
+    }
+    startIndex = delimiterIndex+1;
+    if(DEBUG){
+      Serial.print("command ");
+      Serial.println(command);
+    }
+  }
+}
+void dispense(int dispenserId, int amount) {
+  if(DEBUG){
+    Serial.print("dispenser ");
+    Serial.print(dispenserId);
+    Serial.print(" with amount ");
+    Serial.println(amount);
+  }
+  
+  int servoPin = 0;
+  int motorPin = 0;
 
   switch (dispenserId) {
   case 1:
     servoPin = servoPinFirstDispenser;
     break;
-  default:
-    servoPin = 0;
+  case 2:
+    motorPin = motorPinSecondDispenser;
+    break;
   }
 
   if(servoPin > 0){
@@ -57,8 +92,14 @@ void dispense(int dispenserId, int amount) {
       delay(500); 
     }
   }
-}
 
+  if(motorPin > 0){
+    for(int amountDispensed = 0; amountDispensed < amount; amountDispensed++){
+      runMotor(motorPin);
+      delay(500); 
+    }
+  }
+}
 void runServo(int servoPin){
   servo.attach(servoPin);
   
@@ -73,5 +114,33 @@ void runServo(int servoPin){
     servo.write(servoAngle);          
     delay(10);      
   }
+}
+void runMotor(int motorPin){
+  digitalWrite(motorPin, HIGH);
+
+  long stopCounter = 0;
+  photoResistorValue = analogRead(photoResistorPin);
+  while(photoResistorValue < photoResistorActivateValue && stopCounter < 50000){
+    stopCounter++;
+    photoResistorValue = analogRead(photoResistorPin);
+    
+    if(DEBUG){
+      Serial.print("Motor running, resistor value: ");
+      Serial.print(photoResistorValue);
+      Serial.print(", stopCounter: ");
+      Serial.println(stopCounter);
+    }
+  }
+  stopMotor(motorPin);
+
+  if(DEBUG){
+    Serial.print("Stop, Photo Resistor: ");
+    Serial.print(photoResistorValue);
+    Serial.print(", stopCounter: ");
+    Serial.println(stopCounter);
+  }
+}
+void stopMotor(int motorPin){
+  digitalWrite(motorPin, LOW);
 }
 
